29273c7996d5cf191def45b051e4a2ab5dd06ec9
ShakurUrRahman <shakururrahman@gmail.com>
2026-02-14 17:00:04 +0600

    correction

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -48,7 +48,7 @@ const DashboardPage = async () => {
 				<Card>
 					<CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
 						<CardTitle className="text-sm font-medium">
-							Total Revenue
+							Total Revenue(s)
 						</CardTitle>
 					</CardHeader>
 					<CardContent>
